aa14ab1aee7ce2d1be80ac39f0352799ad1ddfa2
Tony Kordysh <tkordysh@gmail.com>
2022-02-10 15:58:32 -0600

    solved first task

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+    return {name, price, category};
 }
+
+console.log('task 1a', createMenuItem('tacos', 8, 'Lunch'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
@@ -28,6 +30,10 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
+
+console.log('task 1b', createMenuItem("pizza",5,"lunch"));
+console.log('task 1b', createMenuItem("fries",3,"sides"));
+console.log('task 1b', createMenuItem("cookie",2,"dessert"));
 
 
 
